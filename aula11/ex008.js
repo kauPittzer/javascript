fa83fc0bc9710420ac4d5b1c3b6bac5be01ee5bf
KauPittzer <pittzerkauanlopes@gmail.com>
2024-07-07 21:51:05 -0300

    exercicio 8, 9, 10 e 11

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 68.4
+
+console.log(`A velocidade do seu carro é ${vel}Km/h por hora`)
+if(vel > 60) { //Condição Simples
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+}
