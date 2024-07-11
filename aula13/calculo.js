27903544b51771f4e6ed96faf59f6c67e01b66af
KauPittzer <pittzerkauanlopes@gmail.com>
2024-07-10 21:44:27 -0300

    Create calculo.js

---
+++ aula13/calculo.js
@@ -0,0 +1,19 @@
+var c = 1
+do {
+    console.log(`Essa é a mensagem de número ${c}`)
+    c++
+} while(c <= 6)
+
+/*while(c <= 6) {
+    console.log(`Essa é a mensagem de número ${c}`)
+    c++
+}
+*/
+
+/*
+console.log(`Essa é a mensagem de número ${c}`)
+console.log(`Essa é a mensagem de número ${c}`)
+console.log(`Essa é a mensagem de número ${c}`)
+console.log(`Essa é a mensagem de número ${c}`)
+console.log(`Essa é a mensagem de número ${c}`)
+*/
