12e9cd5025a22cb15ad414fa213d576da1669ace
KauPittzer <pittzerkauanlopes@gmail.com>
2024-07-16 01:46:21 -0300

    desafio final de curso javascript

--- desafios/d5/scripts/script.js
+++ desafios/d5/scripts/script.js
@@ -1,16 +1,16 @@
-let tab = document.getElementById('isel')
-let numAdd = document.getElementById('inum')
-let calc = document.getElementById('calc')
-let local = []
+let num = document.querySelector('input#inum')
+let tab = document.querySelector('select#isel')
+let res = document.getElementById('calc')
+let valores = []
 
-function isNum(n) {
-    if(Number(n) >= 1 && Number(n) <= 100){
+//Funções de Verificação
+function isNumero(n) {
+    if(Number(n)>=1 && Number(n)<=100) {
         return true
     }else {
         return false
+    }
 }
-}
-
 function inList(n, l) {
     if(l.indexOf(Number(n)) != -1) {
         return true
@@ -19,27 +19,45 @@ function inList(n, l) {
     }
 }
 
+//Função de Adicionar
 function add() {
-    if(isNum(numAdd.value) && !inList(numAdd.value, local)) {
-        calc.innerHTML = ''
-        local.push(numAdd)
+    if(isNumero(num.value) && !inList(num.value, valores)) {
+        valores.push(Number(num.value))
         let item = document.createElement('option')
-        item.text = `Valor ${numAdd.value} adicionado.`
+        item.text = `Valor ${num.value} foi adicionado`
         tab.appendChild(item)
+        res.innerText = ''
     }else {
-        alert('Valor invalido ou não encontrado na lista!')
+        alert('Valor inválido ou já encontrado na lista.')
     }
-    numAdd.value = ''
-    numAdd.focus()
+    num.value = ''
+    num.focus()
 }
+
+//Função de Resultado
 function final() {
-    if (local.find((Element) => Element > 0)) {
-        calc.innerHTML = ''
-        local.sort()
-        let item = document.createElement('p')
-        item.innerHTML = `Ao todo, temos ${local.length} números cadastrados.`
-        calc.appendChild(item)
+    if(valores.length == 0) {
+        alert('Digite um valor para continuar!')
     }else {
-        alert('Digite um número para continuar')
+        let total = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+        for(let pos in valores) {
+            soma += valores[pos]
+            media = soma/total
+            if( valores[pos] > maior) 
+                maior = valores[pos]
+            if ( valores[pos] < menor) 
+                menor = valores[pos]
+        } 
+
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo, Temos ${total} números cadastrados.</p>`
+        res.innerHTML += `<p>O menor valor encontrado foi ${menor}.</p>`
+        res.innerHTML += `<p>O maior valor encontrado foi ${maior}.</p>`
+        res.innerHTML += `<p>A soma de todos os números é igual a ${soma}.</p>`
+        res.innerHTML += `<p>A média dos números é igual a ${media}.</p>`
     }
 }
